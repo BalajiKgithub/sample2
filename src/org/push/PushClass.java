cad618e4df466f8a644023b80eed76d4bbc6ab64
BalajiKgithub <balajikamarajmallika@gmail.com>
2023-04-02 12:38:18 +0530

    update

--- src/org/push/PushClass.java
+++ src/org/push/PushClass.java
@@ -1,5 +1,23 @@
 package org.push;
 
+
+
 public class PushClass {
+	
+	private void empId() {
+		System.out.println("empId is 12345");
+	}
+	
+	private void empName() {
+		System.out.println("empName is BALA");
+	}
+
+	public static void main(String[] args) {
+
+		PushClass g = new PushClass();
+		g.empId();
+		g.empName();
+	}
+
 
 }
